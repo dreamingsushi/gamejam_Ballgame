e1c525132708c8e4ed286a1556afbbbcfdf88651
Iamth3jack <joelchai1011@gmail.com>
2023-10-22 01:02:20 +0800

    Jack Updated With Coding

---
+++ Assets/Script/SwitchChecker.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchChecker : MonoBehaviour
+{
+    public List<GameObject> switches;
+
+    private SwitchLogic switchLogic;
+
+    private bool AllBooleanCheck = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject obj in switches) 
+        {
+            switchLogic = obj.GetComponent<SwitchLogic>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!switchLogic.isActivated)
+        {
+
+        }
+        else if (switchLogic.isActivated) 
+        {
+            AllBooleanCheck = true;
+        }
+
+        if (AllBooleanCheck)
+        {
+            Debug.Log("Cleared");
+        }
+    }
+}
---
+++ Assets/Script/SwitchLogic.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchLogic : MonoBehaviour
+{
+    public bool isActivated = false;
+
+    private bool canInteract = false;
+
+    public float interactionRange = 2f;
+    /*[SerializeField]
+    private GameObject switcher;
+
+    private int switchState;
+
+    [SerializeField]
+    private Sprite[] switchSprites;
+
+    private Image switchImage; */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*switchState = 0;
+
+        switchImage = GetComponent<Button>().image;
+        switchImage.sprite = switchSprites[switchState];
+
+        gameObject.GetComponent<Button>().onClick.AddListener(TurnOnAndOff); */
+    }
+
+    /* private void TurnOnAndOff()
+    {
+        switchState = 1 - switchState;
+        switchImage.sprite = switchSprites[switchState];
+    } */
+
+    private void OnMouseDown()
+    {
+        if (!isActivated) 
+        {
+            ActivateSwitch();
+        }
+    }
+
+    public void ActivateSwitch()
+    {
+        isActivated = true;
+    }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canInteract = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Other"))
+        {
+            canInteract = false;
+        }
+    } */
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (canInteract && Input.GetKeyDown(KeyCode.E))
+        {
+            if (!isActivated)
+            {
+                ActivateSwitch();
+            }
+        } */
+    }
+}
